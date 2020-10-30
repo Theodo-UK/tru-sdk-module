3e7c024a678cdeaab57cca46f29e47e9b21b712a
Clément Pasteau <clementp@theodo.co.uk>
2020-10-30 15:49:25 +0000

    chore: initial commit

---
+++ android/src/main/java/com/trusdkreactnative/TruSdkReactNativePackage.kt
@@ -0,0 +1,20 @@
+package com.trusdkreactnative
+
+import java.util.Arrays
+import java.util.Collections
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+import com.facebook.react.bridge.JavaScriptModule
+
+class TruSdkReactNativePackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return Arrays.asList<NativeModule>(TruSdkReactNativeModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList<ViewManager<*, *>>()
+    }
+}
